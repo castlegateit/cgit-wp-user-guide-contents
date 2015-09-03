7f6c2356cd5e49ea59855c700c58e317d573e4cc
John Hughes <john@castlegateit.co.uk>
2015-09-03 10:31:24 +0100

    Initial commit

---
+++ cgit-wp-user-guide-contents.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+
+Plugin Name: Castlegate IT WP User Guide Contents
+Plugin URI: http://github.com/castlegateit/cgit-wp-user-guide-contents
+Description: Add a table of contents to the Castlegate IT WordPress User Guide plugin.
+Version: 1.0
+Author: Castlegate IT
+Author URI: http://www.castlegateit.co.uk/
+License: MIT
+
+*/
+
+/**
+ * Enqueue CSS
+ */
+add_action('admin_enqueue_scripts', function($hook) {
+
+    if ($hook != 'toplevel_page_cgit-user-guide') {
+        return;
+    }
+
+    $url = plugin_dir_url(__FILE__);
+
+    wp_enqueue_style(
+        'cgit-wp-user-guide-contents',
+        $url . 'css/user-guide-contents.css'
+    );
+
+});
+
+/**
+ * Generate table of contents
+ *
+ * Extracts the first heading from each section and adds it to the table of
+ * contents. Adds an empty element with the correct ID to allow linking. Also
+ * adds "Back to top" links to the end of each section.
+ *
+ * The table of contents is added to the start of the user guide as another
+ * section.
+ */
+add_filter('cgit_user_guide_sections', function($sections) {
+
+    $pattern = '/<(h[1-6])[^>]*>(.+?)<\/\1>/i';
+    $back = '<p class="cgit-user-guide-contents-back">'
+        . '<a href="#">Back to top</a></p>';
+    $contents = '<h3>Contents</h3>'
+        . '<ol class="cgit-user-guide-contents-list">';
+    $headings = array();
+    $number = 0;
+
+    foreach ($sections as &$section) {
+
+        $match = preg_match($pattern, $section, $matches);
+        $id = 'cgit-user-guide-section-' . $number;
+        $headings[$id] = strip_tags($matches[2]);
+        $section = '<span id="' . $id
+            . '" class="cgit-user-guide-contents-anchor"></span>'
+            . $section . $back;
+
+    }
+
+    if (count($headings) == 0) {
+        return $sections;
+    }
+
+    foreach ($headings as $id => $heading) {
+        $contents .= '<li><a href="#' . $id . '">' . $heading . '</a></li>';
+    }
+
+    $contents .= '</ol>';
+    array_unshift($sections, $contents);
+
+    return $sections;
+
+}, 9999);
